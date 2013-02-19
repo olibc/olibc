codeaurora-unofficial/platform-bionic

Bionic/libm: Prefer branches and VFP ABI
For internal functions set gcc attribute "aapcs-vfp" for ARM
and use -fno-if-conversion to prefer branches over predicated
instructions (improves performance on architectures with good
branch prediction).

Change-Id: I365e9508bd3babb0bb06fc5de127c1ae17445bcc

libm/Android.mk
@@ -165,7 +165,7 @@ ifeq ($(TARGET_ARCH),arm)
   ifeq ($(TARGET_USE_KRAIT_BIONIC_OPTIMIZATION),true)
     libm_common_src_files += \
 	  arm/e_pow.S
-    libm_common_cflags += -DKRAIT_NEON_OPTIMIZATION
+    libm_common_cflags += -DKRAIT_NEON_OPTIMIZATION -fno-if-conversion
   endif
 
   ifeq ($(TARGET_USE_SPARROW_BIONIC_OPTIMIZATION),true)

olibc/olibc

Change-Id: Ie5f751d0971756d0d3ae61f3656eca91a1deb6b4
Signed-off-by: Jim Huang <jserv@0xlab.org>

libm/Android.mk
@@ -223,7 +223,7 @@ libm_common_includes := $(LOCAL_PATH)/upstream-freebsd/lib/msun/src/
 libm_arm_includes := $(LOCAL_PATH)/arm
 libm_arm_src_files := arm/fenv.c
 ifeq ($(TARGET_USE_KRAIT_BIONIC_OPTIMIZATION),true)
-libm_arm_cflags += -DKRAIT_NEON_OPTIMIZATION
+libm_arm_cflags += -DKRAIT_NEON_OPTIMIZATION -fno-if-conversion
 libm_arm_src_files += \
     arm/e_pow.S
 endif