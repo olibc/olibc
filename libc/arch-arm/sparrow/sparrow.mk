28d0065d8ea492473d18e978a49ab7fb6cb3d7de
Kito Cheng <kito@0xlab.org>
2013-03-21 19:19:47 +0800

    Add arch specific versions of strcmp for sparrow and scorpion

--- libc/arch-arm/sparrow/sparrow.mk
+++ libc/arch-arm/sparrow/sparrow.mk
@@ -1,5 +1,6 @@
 $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/krait/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/krait/bionic/memset.S)
+$(call libc-add-cpu-variant-src,STRCMP,arch-arm/krait/bionic/strcmp.S)
 $(call libc-add-cpu-variant-src,MEMMOVE,arch-arm/krait/bionic/memmove.S)
 # bcopy implemented in memmove.S
 $(call libc-add-cpu-variant-src,BCOPY,)
