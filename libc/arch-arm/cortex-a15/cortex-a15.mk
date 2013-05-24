4a59b7ff454762e6a260e10d6239e16bfa673d95
Jim Huang <jserv@0xlab.org>
2013-05-24 14:03:17 +0800

    arch-arm: cortex-a15: Avoid multiple definition of '__strchr_chk'

--- libc/arch-arm/cortex-a15/cortex-a15.mk
+++ libc/arch-arm/cortex-a15/cortex-a15.mk
@@ -2,7 +2,6 @@ $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/cortex-a15/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/cortex-a15/bionic/memset.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/cortex-a15/bionic/strcmp.S)
 $(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a9/bionic/strlen.S)
-$(call libc-add-cpu-variant-src,STRCHR,arch-arm/cortex-a9/bionic/strchr.S)
 $(call libc-add-cpu-variant-src,MEMCHR,arch-arm/cortex-a9/bionic/memchr.S)
 
 include bionic/libc/arch-arm/generic/generic.mk
