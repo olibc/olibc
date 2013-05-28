7f36751bb78a86a4581564ce8e61b8203286afd9
Kito Cheng <kito@0xlab.org>
2013-05-28 11:47:59 +0800

    Add process_vm_writev/process_vm_readv system call wrapper

--- libc/include/sys/uio.h
+++ libc/include/sys/uio.h
@@ -42,6 +42,19 @@ ssize_t preadv(int fd, const struct iovec *iov, int iovcnt,
 ssize_t pwritev(int fd, const struct iovec *iov, int iovcnt,
                 off_t offset);
 
+ssize_t process_vm_readv(pid_t pid,
+                         const struct iovec *local_iov,
+                         unsigned long liovcnt,
+                         const struct iovec *remote_iov,
+                         unsigned long riovcnt,
+                         unsigned long flags);
+ssize_t process_vm_writev(pid_t pid,
+                          const struct iovec *local_iov,
+                          unsigned long liovcnt,
+                          const struct iovec *remote_iov,
+                          unsigned long riovcnt,
+                          unsigned long flags);
+
 __END_DECLS
 
 #endif /* _SYS_UIO_H_ */
