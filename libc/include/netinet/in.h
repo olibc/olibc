98162ef61fc9e51fc1b8af52a938df32f708b808
Kito Cheng <kito@0xlab.org>
2013-06-17 21:50:19 +0800

    Update netinet/in.h for more pre-define constant

--- libc/include/netinet/in.h
+++ libc/include/netinet/in.h
@@ -32,6 +32,7 @@
 #include <netinet/in6.h>
 #include <sys/cdefs.h>
 #include <sys/socket.h>
+#include <sys/_types.h>
 
 #include <linux/in.h>
 #include <linux/in6.h>
@@ -39,9 +40,41 @@
 
 __BEGIN_DECLS
 
-#define IPPORT_RESERVED  1024
+enum {
+    IPPORT_ECHO = 7,
+    IPPORT_DISCARD = 9,
+    IPPORT_SYSTAT = 11,
+    IPPORT_DAYTIME = 13,
+    IPPORT_NETSTAT = 15,
+    IPPORT_FTP = 21,
+    IPPORT_TELNET = 23,
+    IPPORT_SMTP = 25,
+    IPPORT_TIMESERVER = 37,
+    IPPORT_NAMESERVER = 42,
+    IPPORT_WHOIS = 43,
+    IPPORT_MTP = 57,
+    IPPORT_TFTP = 69,
+    IPPORT_RJE = 77,
+    IPPORT_FINGER = 79,
+    IPPORT_TTYLINK = 87,
+    IPPORT_SUPDUP = 95,
+
+    IPPORT_EXECSERVER = 512,
+    IPPORT_LOGINSERVER = 513,
+    IPPORT_CMDSERVER = 514,
+    IPPORT_EFSSERVER = 520,
+
+    IPPORT_BIFFUDP = 512,
+    IPPORT_WHOSERVER = 513,
+    IPPORT_ROUTESERVER = 520,
+
+    IPPORT_RESERVED = 1024,
+    IPPORT_USERRESERVED = 5000
+};
 
 #define INET_ADDRSTRLEN 16
+
+typedef __in_port_t in_port_t;
 
 extern int bindresvport (int sd, struct sockaddr_in *sin);
 
