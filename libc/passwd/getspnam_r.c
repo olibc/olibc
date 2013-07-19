252a9efde7f22b2963f9991a5c74736c10c524bc
Kito Cheng <kito@0xlab.org>
2013-07-20 00:56:25 +0800

    Rewrite getspnam_r

--- libc/passwd/getspnam_r.c
+++ libc/passwd/getspnam_r.c
@@ -15,28 +15,44 @@
  */
 
 #include <shadow.h>
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <paths.h>
 #include "passwd_private.h"
+
+struct spwd_next_func_arg_t {
+  struct spwd *spbuf;
+  char *buf;
+  size_t buflen;
+  struct spwd **spbufp;
+};
+
+static int spwd_find_by_name(void *spwd,
+                             const void *spwd_target)
+{
+  struct spwd *_spwd = (struct spwd *)spwd;
+  const char *_spwd_target = (const char *)spwd_target;
+  return strcmp(_spwd->sp_namp, _spwd_target) == 0;
+}
+
+static void *spwd_next(void *arg)
+{
+  struct spwd_next_func_arg_t *spwd_arg = (struct spwd_next_func_arg_t*)arg;
+  getspent_r(spwd_arg->spbuf, spwd_arg->buf,
+             spwd_arg->buflen, spwd_arg->spbufp);
+  return (void*)(*spwd_arg->spbufp);
+}
+
 
 int getspnam_r(const char *name, struct spwd *spbuf,
                char *buf, size_t buflen, struct spwd **spbufp)
 {
-  int retval;
-  setspent();
-  while (!(retval = getspent_r(spbuf, buf, buflen, spbufp))) {
-    if (strcmp(spbuf->sp_namp, name) == 0) {
-      return 0;
-    }
-  }
-  if (retval) {
-    /* Something wrong in getspent_r */
-    return retval;
-  } else {
-    /* Just not found */
-    return ERANGE;
-  }
+  const char *spwd_target = name;
+  struct spwd_next_func_arg_t spwd_next_func_arg;
+  spwd_next_func_arg.spbuf = spbuf;
+  spwd_next_func_arg.buf = buf;
+  spwd_next_func_arg.buflen = buflen;
+  spwd_next_func_arg.spbufp = spbufp;
+  return __pwdgrp_find(setspent, endspent,
+                       spwd_next, &spwd_next_func_arg,
+                       spwd_find_by_name, spwd_target,
+                       (void**)spbufp);
 }
